de4fb1ff5798d3740995faed693c0aba0f64997b
PranavThorat1432 <pranavthorat95@gmail.com>
2025-09-01 19:20:50 +0530

    Admin Authentication Function

---
+++ Server/middleware/auth.js
@@ -0,0 +1,23 @@
+import { clerkClient } from '@clerk/express';
+
+export const protectAdmin = async (req, res, next) => {
+    try {
+        const { userId } = req.auth();
+        const user = await clerkClient.users.getUser(userId);
+
+        if(user.privateMetadata.role !== 'admin') {
+            return res.json({
+                message: 'Not Authorizerd',
+                success: false
+            })
+        }
+        next();
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({
+            message: error.message,
+            success: false
+        })
+    }
+}
