cfbb3d0e9a6bd29d7827552640aa5d1ff58ebde1
PranavThorat1432 <pranavthorat95@gmail.com>
2025-10-07 16:47:16 +0530

    Booking Route

--- Server/routes/bookingRoutes.js
+++ Server/routes/bookingRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { createBooking, getOccupiedSeats } from '../controllers/bookingController.js';
+import { requireAuth } from '../middleware/auth.js';
 
 const bookingRouter = express.Router();
+
+// Apply authentication middleware to all booking routes
+bookingRouter.use(requireAuth);
 
 bookingRouter.post('/create', createBooking);
 bookingRouter.get('/seats/:showId', getOccupiedSeats);
