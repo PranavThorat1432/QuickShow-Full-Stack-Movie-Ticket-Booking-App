7365143ff03d7230b6e532e67a9e9ac35ec1516c
PranavThorat1432 <pranavthorat95@gmail.com>
2025-10-07 16:59:31 +0530

    MongoDB

--- Server/configs/mongodb.js
+++ Server/configs/mongodb.js
@@ -1,15 +1,39 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-//MongoDB Connection
+// MongoDB Connection
 const connectDB = async () => {
-    mongoose.connect( process.env.MONGODB_URI, {
-        dbName: "QuickShow"
-    }).then(() => {
-        console.log("MongoDB Connected..!")
-    }).catch((err) => {
-        console.log(err)
-    });
-};
+    try {
+        // Exit if MONGODB_URI is not set
+        if (!process.env.MONGODB_URI) {
+            throw new Error('MONGODB_URI environment variable is not set');
+        }
 
+        // Connection options
+        const options = {
+            dbName: 'QuickShow',
+            serverSelectionTimeoutMS: 10000, // 10 seconds timeout
+            socketTimeoutMS: 45000, // 45 seconds socket timeout
+            maxPoolSize: 10, // Maintain up to 10 socket connections
+            connectTimeoutMS: 10000, // 10 seconds connection timeout
+        };
+
+        // Connect to MongoDB
+        await mongoose.connect(process.env.MONGODB_URI, options);
+        
+        // Log successful connection
+        console.log('MongoDB Connected Successfully');
+        
+        // Log any connection errors after initial connection was established
+        mongoose.connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+        });
+        
+        return mongoose.connection;
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
 
 export default connectDB;
